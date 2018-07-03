dc4e58f9e093a134d67c3e2b41266cb73d3f0abc
Totemika <rosanarufer@gmail.com>
2018-07-03 13:53:56 +0100

    Red: Returns casted single-digit number from string

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,51 +1,22 @@
 
 var stringCalculator = function(){
 
-    function stringSum(expression){
-        var stringNumbers = split(expression, ',');
-        var numbers = clean(stringNumbers);
-        return sumValues(numbers);
-    }
-
-    function split(expression, separator){
-        return expression.split(separator);
-    }
-
-    function clean(collection){
-        return collection.filter(function(b) {
-            return Number(b);
-        })
-    }
-
-    function sumValues(stringNumbers){
-        return stringNumbers.reduce(function(acc, value) {
-            return acc + Number(value);
-        }, 0)
+    function sum(){
     }
 
     return {
-        stringSum: stringSum
+        sum: sum
     }
-
 
 }();
 
-describe('Test the stringSum function', function(){
-    test('Sum two comma separated numbers', function(){
-        expect(stringCalculator.stringSum("1,2")).toBe(3);
+describe('', function(){
+
+    test('Returns casted single-digit number from string', function() {
+        expect(stringCalculator.sum("1")).toBe(1);
     });
 
-    test('Sum a single two-digit number', function(){
-        expect(stringCalculator.stringSum("10")).toBe(10);
-    });
 
-    test('Sum a comma separated set of characters', function(){
-        expect(stringCalculator.stringSum("1,a,A,45,-")).toBe(46);
-    });
-
-    test('Sum with two consecutive commas', function(){
-        expect(stringCalculator.stringSum("1,,5,e")).toBe(6);
-    });
 });
 
 
