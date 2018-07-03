c934a5fb4f14f514b474d4ce58049cff4ccb1457
Totemika <rosanarufer@gmail.com>
2018-07-03 14:19:48 +0100

    Refactor, no need to check if expression is number

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,19 +1,13 @@
+var stringCalculator = function () {
 
-var stringCalculator = function(){
-
-    function sum(expression){
-        var number = Number(expression);
-        if(number){
-            return number;
-        } else {
-            var characters = expression.split(',');
-            var result = 0;
-            for (var i = 0; i < characters.length; i++){
-                var castedNumber = Number(characters[i]);
-                result = (castedNumber) ? result + castedNumber : result;
-            }
-            return result;
+    function sum(expression) {
+        var characters = expression.split(',');
+        var result = 0;
+        for (var i = 0; i < characters.length; i++) {
+            var castedNumber = Number(characters[i]);
+            result = (castedNumber) ? result + castedNumber : result;
         }
+        return result;
     }
 
     return {
@@ -22,18 +16,18 @@ var stringCalculator = function(){
 
 }();
 
-describe('', function(){
+describe('', function () {
 
-    test('Returns casted single-digit number from string', function() {
+    test('Returns casted single-digit number from string', function () {
         expect(stringCalculator.sum("1")).toBe(1);
     });
 
-    test('Sum numbers separated by comma', function() {
+    test('Sum numbers separated by comma', function () {
         expect(stringCalculator.sum("1,2")).toBe(3);
         expect(stringCalculator.sum("1,2,1")).toBe(4);
     });
 
-    test('Return casted number and ignore character', function() {
+    test('Return casted number and ignore character', function () {
         expect(stringCalculator.sum("1,a")).toBe(1);
     });
 
