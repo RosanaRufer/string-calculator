9d24bdfc13a19a91d602399f6495ada2f3ac6617
Totemika <rosanarufer@gmail.com>
2018-07-03 14:05:49 +0100

    Green: Sum two numbers separated by comma

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,8 +1,14 @@
 
 var stringCalculator = function(){
 
-    function sum(){
-        return 1;
+    function sum(expression){
+        var number = Number(expression);
+        if(number){
+            return number;
+        } else {
+            var characters = expression.split(',');
+            return Number(characters[0]) + Number(characters[1]);
+        }
     }
 
     return {
@@ -15,6 +21,10 @@ describe('', function(){
 
     test('Returns casted single-digit number from string', function() {
         expect(stringCalculator.sum("1")).toBe(1);
+    });
+
+    test('Sum numbers separated by comma', function() {
+        expect(stringCalculator.sum("1,2")).toBe(3);
     });
 
 
